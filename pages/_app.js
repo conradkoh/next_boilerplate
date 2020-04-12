6a325ac4c33ce75be589eb6f4822fab4860b40b3
Conrad Koh <conradkoh@gmail.com>
2020-04-12 15:47:12 +0800

    Updated default app.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,36 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/global.css';
 import { ThemeProvider } from '@material-ui/core/styles';
 import theme from '../styles/theme/mui';
-export default function MyApp({ Component, pageProps }) {
+import { Provider } from 'react-redux';
+import React from 'react';
+import PropTypes from 'prop-types';
+import CssBaseline from '@material-ui/core/CssBaseline';
+
+export function MyApp(props) {
+    const { Component, pageProps } = props;
+
+    React.useEffect(() => {
+        // Remove the server-side injected CSS.
+        const jssStyles = document.querySelector('#jss-server-side');
+        if (jssStyles) {
+            jssStyles.parentElement.removeChild(jssStyles);
+        }
+    }, []);
+
     return (
-        <ThemeProvider theme={theme}>
-            <Component {...pageProps} />
-        </ThemeProvider>
+        <React.Fragment>
+            <ThemeProvider theme={theme}>
+                {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+                <CssBaseline />
+                <Provider store={props.store}>
+                    <Component {...pageProps} />
+                </Provider>
+            </ThemeProvider>
+        </React.Fragment>
     );
 }
+
+MyApp.propTypes = {
+    Component: PropTypes.elementType.isRequired,
+    pageProps: PropTypes.object.isRequired,
+};
