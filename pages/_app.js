ee302f328fe7267a033a04404675c69a8935038a
Conrad Koh <conradkoh@gmail.com>
2020-04-12 09:44:03 +0800

    Reverted to next-css

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/global.css';
+import { ThemeProvider } from '@material-ui/core/styles';
+import theme from '../styles/theme/mui';
 export default function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />;
+    return (
+        <ThemeProvider theme={theme}>
+            <Component {...pageProps} />
+        </ThemeProvider>
+    );
 }
