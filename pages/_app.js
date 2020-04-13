f3c1493cb8a0fd87d01e9f0c21062ded2a8cddbd
Conrad Koh <conradkoh@gmail.com>
2020-04-13 11:13:35 +0800

    Fixed default _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,12 +2,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/global.css';
 import { ThemeProvider } from '@material-ui/core/styles';
 import theme from '../styles/theme/mui';
-import { Provider } from 'react-redux';
 import React from 'react';
 import PropTypes from 'prop-types';
 import CssBaseline from '@material-ui/core/CssBaseline';
 
-export function MyApp(props) {
+export default function MyApp(props) {
     const { Component, pageProps } = props;
 
     React.useEffect(() => {
@@ -23,9 +22,7 @@ export function MyApp(props) {
             <ThemeProvider theme={theme}>
                 {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
                 <CssBaseline />
-                <Provider store={props.store}>
-                    <Component {...pageProps} />
-                </Provider>
+                <Component {...pageProps} />
             </ThemeProvider>
         </React.Fragment>
     );
